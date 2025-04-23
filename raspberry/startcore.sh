582088f2abeecf2165d68e21cc0d857be264e17e
jeries-123 <119804522+jeries-123@users.noreply.github.com>
2025-04-23 12:31:37 +0300

    setup is done

--- raspberry/startcore.sh
+++ raspberry/startcore.sh
@@ -9,3 +9,10 @@ python app.py &
 
 # Start the stream and tunnel
 ./stream_tunnel.sh &
+
+
+# sudo systemctl daemon-reload
+# sudo systemctl enable bees-camera
+# sudo systemctl start bees-camera
+# sudo systemctl status bees-camera
+# journalctl -u bees-camera -f
