58c884f70eca4df5e7348cc8e17c0ebe88b9deae
jeries-123 <119804522+jeries-123@users.noreply.github.com>
2025-04-25 17:29:39 +0300

    open issue

--- raspberry/sensors/ir.py
+++ raspberry/sensors/ir.py
@@ -5,7 +5,7 @@ IR_SENSOR = 9
 def read_ir_door_status():
     try:
         state = GPIO.input(IR_SENSOR)
-        print(f"🚪 IR Sensor: {'OPEN(HIGH)' if state else 'CLOSED (LOW)'}")
+        print(f"🚪 IR Sensor: {'CLOSED (HIGH)' if state else 'OPEN(LOW)'}")
         return state == GPIO.HIGH
     except Exception as e:
         print(f"⚠️ Error reading IR sensor: {e}")
