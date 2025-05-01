4ea1bbb80b8f43e6c8746a0a0c36027037c0206b
jeries-123 <119804522+jeries-123@users.noreply.github.com>
2025-05-01 11:57:22 +0300

    still sebuging weight sensor

--- raspberry/app.py
+++ raspberry/app.py
@@ -40,7 +40,7 @@ def main():
             temperature, humidity = get_temp_humidity()
             sound = monitor_sound()
             door_open = read_ir_door_status()
-            weight = get_weight(hx, 10) if hx else 0
+            # weight = get_weight(hx, 10) if hx else 0
 
             # Get and send GPS location
             latitude, longitude = get_gps_location()
