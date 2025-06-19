eae73faf9faa4a117883fe3188ba19f017db277b
jeries-123 <jeriest59@gmail.com>
2025-06-19 12:22:08 +0300

    changing duration to 60s

--- raspberry/app.py
+++ raspberry/app.py
@@ -82,7 +82,7 @@ def main():
 
             send_data_to_api(sensor_data)
             print("🔄 Waiting for next cycle...\n")
-            time.sleep(20)
+            time.sleep(60)
 
     except KeyboardInterrupt:
         print("🛑 Program stopped by user.")
